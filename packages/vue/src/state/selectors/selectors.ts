85291a16cda25af71d78b0dfb74403e4ab2bfc28
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2026-04-12 01:31:02 +0800

    fix(types): widen selectActiveLabel return type to match combineActiveLabel
    combineActiveLabel returns `string | number | undefined` (since TickItem.value
    can be a number), but selectActiveLabel was annotated as `string | undefined`,
    causing TS2322. Also update .gitignore.

--- packages/vue/src/state/selectors/selectors.ts
+++ packages/vue/src/state/selectors/selectors.ts
@@ -134,7 +134,7 @@ export const selectActiveLabel: (
   tooltipEventType: TooltipEventType,
   trigger: TooltipTrigger,
   defaultIndex: TooltipIndex | undefined,
-) => string | undefined = createSelector(selectTooltipAxisTicks, selectActiveIndex, combineActiveLabel)
+) => string | number | undefined = createSelector(selectTooltipAxisTicks, selectActiveIndex, combineActiveLabel)
 
 export const selectTooltipPayload: (
   state: RechartsRootState,
